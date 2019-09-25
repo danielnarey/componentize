c09c80a0ddfca2354c98b73bf2e2aede3e3424f4
Daniel Narey <danielnarey@users.noreply.github.com>
2019-09-25 11:25:08 -0400

    sync

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # @danielnarey/componentize [![Build Status](https://travis-ci.com/danielnarey/componentize.svg?branch=master)](https://travis-ci.com/danielnarey/componentize) [![npm (scoped)](https://img.shields.io/npm/v/@danielnarey/componentize)](https://www.npmjs.com/package/@danielnarey/componentize) ![npm bundle size (scoped)](https://img.shields.io/bundlephobia/min/@danielnarey/componentize)
 
-**Create an updatable component from any function that generates HTML**
+*Create an updatable component from any function that generates HTML*
 
 ## Features
 
--- test/index.js
+++ test/index.js
@@ -1,6 +1,26 @@
 import test from 'ava';
+import {
+  addListeners,
+  setMergeable,
+  setStatic,
+  setUpdatable,
+} from './dist/index.cjs';
 
 
-test('pass', (t) => {
-  t.true(true)
+const doc = new DocumentFragment();
+const rootElem = doc.createElement('div').setAttribute('id', 'root');
+doc.appendChild(root);
+
+
+test('setStatic', (t) => {
+  setStatic(
+    doc,
+    'root',
+    (name) => `<p id="pElem">Hello,<span>${name}</span></p>`,
+    'Daniel',
+  );
+  
+  const pElem = doc.getElementById('pElem');
+  
+  t.is(pElem.innerHTML, 'Hello,<span>Daniel</span>');
 });
