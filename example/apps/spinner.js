ea4fb7605ea24bd63b5d6388b22eec18ff927641
Daniel Narey <danielnarey@users.noreply.github.com>
2019-09-24 22:48:44 -0400

    sync

--- example/apps/spinner.js
+++ example/apps/spinner.js
@@ -29,7 +29,7 @@ const spinner = (w, rootId) => {
     (a, b) => ((a + b >= 360) ? (a + b - 360) : a + b),
   );
 
-  const rate = (1 / 10000) * (1 / 360);
+  const rate = (1 / 1000) * (1 / 360);
   let start;
 
   const transition = (now) => {
